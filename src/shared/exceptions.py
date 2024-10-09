a1e96473e09e23947b50626a95a10b1b419ef237
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-10-09 07:47:29 -0300

    feat: add address exception custom

--- src/shared/exceptions.py
+++ src/shared/exceptions.py
@@ -6,3 +6,13 @@ class UserExistsError(Exception):
 class UserNotFoundError(Exception):
     def __init__(self, user):
         super().__init__(f"O usuário {user} não existe")
+
+
+class AddressExistsError(Exception):
+    def __init__(self, address):
+        super().__init__(f"O endereço {address} ja existe")
+
+
+class AddressNotFoundError(Exception):
+    def __init__(self, address):
+        super().__init__(f"O endereço {address} não existe")
