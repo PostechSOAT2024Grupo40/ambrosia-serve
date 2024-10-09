e2d376273a7f8cbb9387696edd9b693cfcf8cb39
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-10-09 07:46:50 -0300

    feat: add address ports

--- src/ports/presenters/users_presenter.py
+++ src/ports/presenters/users_presenter.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import Any, List
 
 from src.domain.entities.user import User
@@ -6,5 +6,6 @@ from src.domain.entities.user import User
 
 class IUserPresenter(ABC):
 
+    @abstractmethod
     def user_present(self, output: User | List[User]) -> Any:
         ...
