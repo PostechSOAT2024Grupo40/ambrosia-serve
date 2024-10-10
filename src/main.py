5bc6020edb5c171adf89a67abe97bd60381f6e2b
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-10-10 07:50:15 -0300

    chore: test call product controller

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
+from src.product.product_controller import ProductController
+from src.shared.dtos.create_product_request_dto import CreateProductRequestDto
+from src.shared.enums.categories import Categories
+
+
 def main():
-    print("Hello from ambrosia-serve!")
+    request = CreateProductRequestDto(
+        description="Produto 1",
+        price=10.00,
+        stock=100,
+        category=Categories.LANCHE
+    )
+    response = ProductController.create_product(request_data=request.model_dump())
+    print(f"{response=}")
 
 
 if __name__ == "__main__":
