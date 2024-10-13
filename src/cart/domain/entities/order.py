495451accf54c57d4f93d9093e51ab7eb33840d5
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-10-13 09:35:53 -0300

    feat: add order entity

--- src/cart/domain/entities/order.py
+++ src/cart/domain/entities/order.py
@@ -1,38 +1,45 @@
-import uuid
 from datetime import datetime
 from typing import List
 
 from src.cart.domain.enums.order_status import OrderStatus
 from src.cart.domain.enums.paymentConditions import PaymentConditions
+from src.cart.domain.object_values import generate_id
 from src.cart.domain.validators.order_validator import OrderValidator
+from src.product.domain.entities.product import Product
 
 
 class Order:
     def __init__(self,
                  user: int,
-                 user_address: int,
                  total_order: float,
-                 delivery_value: float,
                  product_quantity: int,
                  order_datetime: datetime,
                  order_status: OrderStatus,
                  payment_condition: PaymentConditions,
-                 order_items: List = None):
-        self.id = str(uuid.uuid4())
+                 _order_items: List[Product],
+                 _id: str = generate_id()):
+        self._id = _id
         self.user = user
-        self.user_address = user_address
         self.total_order = total_order
-        self.delivery_value = delivery_value
         self.product_quantity = product_quantity
         self.order_datetime = order_datetime
         self.order_status = order_status
         self.payment_condition = payment_condition
-        self.order_items = order_items if order_items is not None else []
+        self._order_items = _order_items
 
         OrderValidator.validate(order=self)
+        OrderValidator.validate_order_items(order_items=_order_items)
 
     def __hash__(self):
         return hash(self.id)
 
     def __eq__(self, other):
         return self.id == other.id
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def order_items(self):
+        return self._order_items
