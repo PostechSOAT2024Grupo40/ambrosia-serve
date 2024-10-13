a367948a5c9ac6fff1491df57c5a3d5ed7158759
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-10-13 09:55:11 -0300

    refactor: remove protected param

--- src/cart/domain/entities/order.py
+++ src/cart/domain/entities/order.py
@@ -16,7 +16,7 @@ class Order:
                  order_datetime: datetime,
                  order_status: OrderStatus,
                  payment_condition: PaymentConditions,
-                 _order_items: List[Product],
+                 order_items: List[Product],
                  _id: str = generate_id()):
         self._id = _id
         self.user = user
@@ -25,10 +25,10 @@ class Order:
         self.order_datetime = order_datetime
         self.order_status = order_status
         self.payment_condition = payment_condition
-        self._order_items = _order_items
+        self.order_items = order_items
 
         OrderValidator.validate(order=self)
-        OrderValidator.validate_order_items(order_items=_order_items)
+        OrderValidator.validate_order_items(order_items=order_items)
 
     def __hash__(self):
         return hash(self.id)
@@ -39,7 +39,3 @@ class Order:
     @property
     def id(self):
         return self._id
-
-    @property
-    def order_items(self):
-        return self._order_items
