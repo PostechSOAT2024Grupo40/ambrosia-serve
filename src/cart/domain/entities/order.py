b65cb304dd20ce48088f170f8e08930fda03d726
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-10-13 10:06:29 -0300

    refactor: move products into new entity and remove delivery params

--- src/cart/domain/entities/order.py
+++ src/cart/domain/entities/order.py
@@ -1,34 +1,29 @@
 from datetime import datetime
 from typing import List
 
+from src.cart.domain.entities.order_product import OrderProduct
 from src.cart.domain.enums.order_status import OrderStatus
 from src.cart.domain.enums.paymentConditions import PaymentConditions
 from src.cart.domain.object_values import generate_id
 from src.cart.domain.validators.order_validator import OrderValidator
-from src.product.domain.entities.product import Product
 
 
 class Order:
     def __init__(self,
                  user: int,
                  total_order: float,
-                 product_quantity: int,
                  order_datetime: datetime,
                  order_status: OrderStatus,
                  payment_condition: PaymentConditions,
-                 order_items: List[Product],
+                 products: List[OrderProduct],
                  _id: str = generate_id()):
         self._id = _id
         self.user = user
         self.total_order = total_order
-        self.product_quantity = product_quantity
         self.order_datetime = order_datetime
         self.order_status = order_status
         self.payment_condition = payment_condition
-        self.order_items = order_items
-
         OrderValidator.validate(order=self)
-        OrderValidator.validate_order_items(order_items=order_items)
 
     def __hash__(self):
         return hash(self.id)
