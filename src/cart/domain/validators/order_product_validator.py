75e380fd79ee35eaa026deb2cef8118e5c11f4b1
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-10-13 18:34:06 -0300

    feat: add obs validation

--- src/cart/domain/validators/order_product_validator.py
+++ src/cart/domain/validators/order_product_validator.py
@@ -1,6 +1,8 @@
 from src.cart.domain.domain_exception import OrderProductDomainException
 from src.product.domain.entities.product import Product
 
+TEXT_MAX_SIZE = 100
+
 
 class OrderProductValidator:
     @staticmethod
@@ -13,3 +15,9 @@ class OrderProductValidator:
 
         if order_product.product.price <= 0:
             raise OrderProductDomainException("Preço do produto precisa ser maior que zero")
+
+        if order_product.product.stock < order_product.quantity:
+            raise OrderProductDomainException("Produto sem estoque")
+
+        if order_product.observation.strip() and len(order_product.observation) > TEXT_MAX_SIZE:
+            raise OrderProductDomainException("Campo observação tem um tamanho máximo de 100 caracteres")
