f8dfa222dcf1e43d7ce86297f8e9cfaee2ab9f46
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-10-15 00:04:16 -0300

    feat: specify return type

--- src/api/presentation/routes/client/client.py
+++ src/api/presentation/routes/client/client.py
@@ -1,26 +1,33 @@
 from fastapi import APIRouter
+
+from src.api.presentation.shared.dtos.client_response_dto import ClientResponseDto
 from src.api.presentation.shared.dtos.create_user_request_dtos import CreateUserRequestDTO
 from src.client.user_controller import UserController
 
 router = APIRouter()
 
+
 @router.post("/api/v1/user")
-async def create_user(create_user: CreateUserRequestDTO):
+async def create_user(create_user: CreateUserRequestDTO) -> ClientResponseDto:
     create_user_dict = create_user.dict()
     return UserController.create_user(request_data=create_user_dict)
 
+
 @router.get("/api/v1/users")
-async def get_users():
+async def get_users() -> list[ClientResponseDto]:
     return UserController.get_users()
 
+
 @router.get("/api/v1/user/{cpf}")
-async def get_user_by_cpf(cpf: str):
+async def get_user_by_cpf(cpf: str) -> ClientResponseDto:
     return UserController.get_user_by_cpf(cpf)
 
+
 @router.put("/api/v1/user/")
-async def update_user(update_user: CreateUserRequestDTO):
+async def update_user(update_user: CreateUserRequestDTO) -> ClientResponseDto:
     return UserController.update_user(update_user)
 
+
 @router.delete("/api/v1/user/{id}")
-async def delete_user(id: str):
+async def delete_user(id: str) -> bool:
     return UserController.delete_user(id)
