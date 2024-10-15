bf0bc5e240caa392900f616a31b6a9c4ecf2d817
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-10-14 23:25:29 -0300

    fix: import path

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import uvicorn
 
+from src.api.presentation.http import http  # noqa
+
 if __name__ == "__main__":
     uvicorn.run("src.api.presentation.http:http.app", host="127.0.0.1", port=8000, reload=True)
