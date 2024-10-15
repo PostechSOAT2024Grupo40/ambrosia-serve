e308479125592a7249cd61ef1bbef339575e0455
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-10-14 22:23:21 -0300

    feat: add sqlalchemy repository

---
+++ src/product/adapters/postgresql_repository.py
@@ -0,0 +1,38 @@
+from typing import Dict, Any, List, Sequence
+
+from sqlalchemy import select, Row, delete
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.orm import Session
+
+from src.product.adapters.product_table import ProductTable
+from src.product.ports.repository_interface import IRepository
+
+
+class PostgreSqlRepository(IRepository):
+    def __init__(self, session: Session):
+        super().__init__()
+        self.session = session
+
+    def get_all(self) -> List[Dict]:
+        stmt = select(ProductTable)
+        results: Sequence[Row[tuple[ProductTable]]] = self.session.execute(stmt).all()
+        if not results:
+            return []
+
+        return [row.to_dict() for row in results]
+
+    def filter_by_sku(self, sku: str) -> Dict:
+        stmt = select(ProductTable).where(ProductTable.sku == sku)
+        result = self.session.execute(stmt).first()
+        if not result:
+            return {}
+
+        return result[0].to_dict()
+
+    def insert_update(self, values: Dict[str, Any]):
+        stmt = insert(ProductTable).values(**values)
+        self.session.execute(stmt)
+
+    def delete(self, sku: str):
+        stmt = delete(ProductTable).where(ProductTable.sku == sku)
+        self.session.execute(stmt)
