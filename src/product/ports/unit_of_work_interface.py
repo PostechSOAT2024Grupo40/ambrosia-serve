4b7304be5600e5098b7e21609e257aed6a62da28
rafixcs <rafael.camargo.rs.@gmail.com>
2024-10-15 17:53:36 -0300

    added mock for webhook payment

--- src/product/ports/unit_of_work_interface.py
+++ src/product/ports/unit_of_work_interface.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-from product.ports.repository_interface import IProductRepository
+#from product.ports.repository_interface import IProductRepository
 
 
 class IProductUnitOfWork(ABC):
