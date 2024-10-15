8e9e9ca9e6f3eedb6d2b0b39ece144a1486d21f3
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-10-14 23:24:58 -0300

    feat: implement pydantic presenter

---
+++ src/product/adapters/pydantic_presenter.py
@@ -0,0 +1,18 @@
+from typing import List
+
+from src.api.presentation.shared.dtos.product_response_dto import ProductResponseDto
+from src.product.domain.entities.product import Product
+from src.product.ports.product_presenter import IProductPresenter
+
+
+class PydanticProductPresenter(IProductPresenter):
+    def present(self, output: Product | List[Product]) -> ProductResponseDto | List[ProductResponseDto]:
+        if isinstance(output, list):
+            return [self.formater(p) for p in output]
+        return self.formater(output)
+
+    @staticmethod
+    def formater(p):
+        if not p:
+            return {}
+        return ProductResponseDto(**p)
