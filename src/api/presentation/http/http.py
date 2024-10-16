e952ea75f82228f89b86aa2502da2187f4bc0444
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-10-15 23:05:23 -0300

    feat: add health endpoint

--- src/api/presentation/http/http.py
+++ src/api/presentation/http/http.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+
+from src.api.presentation.routes.health import health
 from src.api.presentation.routes.products import products
 from src.api.presentation.routes.cart import cart
 from src.api.presentation.routes.client import client
@@ -7,3 +9,5 @@ app = FastAPI()
 app.include_router(products.router, tags=["products"])
 app.include_router(client.router, tags=["users"])
 app.include_router(cart.router, tags=["cart"])
+
+app.include_router(health.router, tags=["health"])
