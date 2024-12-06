791151b1eafdcdb6a7c2cfc2551ba486098a70b8
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-12-06 09:02:19 -0300

    test: implement product gateway tests with mock

--- tests/product/adapters/conftest.py
+++ tests/product/adapters/conftest.py
@@ -1,27 +1,21 @@
+from unittest.mock import MagicMock
+
 import pytest
-from testcontainers.postgres import PostgresContainer
+
+from src.product.adapters.postgres_gateway import PostgreSqlProductGateway
 
 
-@pytest.fixture(scope="module")
-def postgres_container():
-    """Start a PostgreSQL container."""
-    with PostgresContainer("postgres:15.2") as postgres:
-        yield postgres
+@pytest.fixture
+def mock_uow():
+    uow = MagicMock()
+    uow.repository.get_all = MagicMock()
+    uow.repository.filter_by_sku = MagicMock()
+    uow.repository.insert_update = MagicMock()
+    uow.repository.delete = MagicMock()
+    uow.commit = MagicMock()
+    return uow
 
 
-@pytest.fixture(scope="module")
-def db_connection(postgres_container):
-    """Set up a database connection and initialize schema."""
-    conn = psycopg2.connect(
-        dbname=postgres_container.get_database_name(),
-        user=postgres_container.get_username(),
-        password=postgres_container.get_password(),
-        host=postgres_container.get_container_host_ip(),
-        port=postgres_container.get_exposed_port(5432),
-    )
-    conn.autocommit = True
-    with conn.cursor() as cursor:
-        cursor.execute(open("products_schema.sql", "r").read())
-    yield conn
-    cursor.close()
-    conn.close()
+@pytest.fixture
+def gateway(mock_uow):
+    return PostgreSqlProductGateway(mock_uow)
