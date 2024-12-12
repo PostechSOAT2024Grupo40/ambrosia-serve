7f9045ba4ba94a620a09477f7822a60d906c35a7
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-12-11 22:31:44 -0300

    feat: add init and repr to class

--- src/cart/adapters/order_table.py
+++ src/cart/adapters/order_table.py
@@ -18,6 +18,23 @@ class OrderTable(Base):
     created_at: Mapped[DateTime] = mapped_column(default=now())
     updated_at: Mapped[DateTime] = mapped_column(default=now(), onupdate=now())
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.id = kwargs.get('id')
+        self.user_id = kwargs.get('user_id')
+        self.status = kwargs.get('status')
+        self.payment_condition = kwargs.get('payment_condition')
+        self.created_at = kwargs.get('created_at')
+        self.updated_at = kwargs.get('updated_at')
+
+    def __repr__(self):
+        return (f"OrderTable(id={self.id}, "
+                f"user_id={self.user_id}, "
+                f"status={self.status}, "
+                f"payment_condition={self.payment_condition}, "
+                f"created_at={self.created_at}, "
+                f"updated_at={self.updated_at})")
+
 
 class OrderProductTable(Base):
     __tablename__ = "order_products"
@@ -32,3 +49,21 @@ class OrderProductTable(Base):
     order_id: Mapped[str] = mapped_column(ForeignKey("orders.id"))
     order = relationship("OrderTable", back_populates="products")
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.id = kwargs.get('id')
+        self.product_id = kwargs.get('product_id')
+        self.quantity = kwargs.get('quantity')
+        self.observation = kwargs.get('observation')
+        self.created_at = kwargs.get('created_at')
+        self.updated_at = kwargs.get('updated_at')
+        self.order_id = kwargs.get('order_id')
+
+    def __repr__(self):
+        return (f"OrderProductTable(id={self.id}, "
+                f"product_id={self.product_id}, "
+                f"quantity={self.quantity}, "
+                f"observation={self.observation}, "
+                f"created_at={self.created_at}, "
+                f"updated_at={self.updated_at}, "
+                f"order_id={self.order_id})")
