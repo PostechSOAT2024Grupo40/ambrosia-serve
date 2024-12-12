023420a78b8b627177895f4a6c4bc416c40a3556
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-12-11 22:09:55 -0300

    refactor: remove create db

--- src/api/presentation/http/http.py
+++ src/api/presentation/http/http.py
@@ -4,9 +4,9 @@ from src.api.presentation.routes.cart import cart
 from src.api.presentation.routes.client import client
 from src.api.presentation.routes.health import health
 from src.api.presentation.routes.products import products
-from src.shared.postgresql_session_factory import create_db
 
 app = FastAPI()
+
 app.include_router(products.router, tags=["products"])
 app.include_router(client.router, tags=["users"])
 app.include_router(cart.router, tags=["cart"])
@@ -16,5 +16,4 @@ app.include_router(health.router, tags=["health"])
 if __name__ == "__main__":
     import uvicorn
 
-    create_db()
-    uvicorn.run("app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
