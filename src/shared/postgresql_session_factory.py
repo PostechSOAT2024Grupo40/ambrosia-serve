588b09a20ecdfdacc2f4b42a28c1c7179b2f8386
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-12-11 22:28:53 -0300

    refactor: remove create tables

--- src/shared/postgresql_session_factory.py
+++ src/shared/postgresql_session_factory.py
@@ -3,10 +3,6 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 
-from src.cart.adapters.order_table import Base as OrderBase
-from src.client.adapters.client_table import Base as ClientBase
-from src.product.adapters.product_table import Base as ProductBase
-
 
 def get_db_url():
     dbname = os.environ['POSTGRES_DB']
@@ -16,15 +12,6 @@ def get_db_url():
     password = os.environ['POSTGRES_PASSWORD']
 
     return create_engine(f"postgresql://{user}:{password}@{host}:{port}/{dbname}")
-
-
-def create_db():
-    try:
-        all_bases = [ProductBase, ClientBase, OrderBase]
-        for base in all_bases:
-            base.metadata.create_all(get_db_url(), checkfirst=True)
-    except Exception as e:
-        print(f"Erro ao conectar ao banco de dados: {e}")
 
 
 def postgresql_session_factory() -> sessionmaker[Session]:
