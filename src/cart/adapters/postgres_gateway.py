4735c46f9b85dc1f6da1c6b2ecb5e08a9a4977a5
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-12-11 22:10:54 -0300

    refactor: transform row type to entity

--- src/cart/adapters/postgres_gateway.py
+++ src/cart/adapters/postgres_gateway.py
@@ -1,11 +1,11 @@
-from typing import List
+from typing import List, Optional
+
+from sqlalchemy import Row
 
 from src.cart.domain.entities.order import Order
-from src.cart.domain.entities.order_product import OrderProduct
 from src.cart.domain.enums.paymentConditions import PaymentConditions
 from src.cart.ports.cart_gateway import ICartGateway
 from src.cart.ports.unit_of_work_interface import ICartUnitOfWork
-from src.product.domain.entities.product import Product
 
 
 class PostgreSqlOrderGateway(ICartGateway):
@@ -14,9 +14,19 @@ class PostgreSqlOrderGateway(ICartGateway):
         super().__init__()
         self.uow = uow
 
+    def get_order_products(self, order_id) -> List[dict]:
+        with self.uow:
+            order_products: List[Row] = self.uow.repository.get_order_products(order_id)
+            return [{
+                'id': p.id,
+                'product_id': p.product_id,
+                'quantity': p.quantity,
+                'observation': p.observation
+            } for p in order_products]
+
     def get_orders(self) -> List[Order]:
         with self.uow:
-            orders = self.uow.repository.get_all()
+            orders: Optional[List[Row]] = self.uow.repository.get_all()
             return [self.build_order_entity(o) for o in orders]
 
     def get_order_by_id(self, order_id: str) -> Order:
@@ -49,24 +59,11 @@ class PostgreSqlOrderGateway(ICartGateway):
     def build_order_entity(order):
         if not order:
             return None
-        payment_condition = PaymentConditions[order['payment_condition']]
-        products = []
-        for p in order['products']:
-            products.append(
-                OrderProduct(
-                    product=Product(_id=p['id'],
-                                    sku=p['sku'],
-                                    description=p['description'],
-                                    category=p['category'],
-                                    stock=p['stock'],
-                                    price=p['price']),
-                    quantity=p['quantity'],
-                    observation=p['observation']
-                )
-            )
-        return Order(_id=order['id'],
-                     user=order['user_id'],
-                     order_datetime=order['created_at'],
-                     order_status=order['status'],
-                     payment_condition=payment_condition.value,
-                     products=products)
+
+        payment_condition = PaymentConditions[order.payment_condition]
+
+        return Order(_id=order.order_id,
+                     user=order.user_id,
+                     order_datetime=order.created_at,
+                     order_status=order.status,
+                     payment_condition=payment_condition.value)
