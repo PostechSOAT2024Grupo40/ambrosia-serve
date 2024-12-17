61965776886417a4eabcb3c363df2ba28662fa58
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-12-16 21:53:27 -0300

    fix: base class

--- src/client/adapters/client_table.py
+++ src/client/adapters/client_table.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 from sqlalchemy.sql.functions import now
 
 
-class Base:
+class Base(DeclarativeBase):
     pass
 
 
