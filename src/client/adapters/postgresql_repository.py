5f9d3b4548b4bd28f52089d7fb6596a0de895b97
Rodrigo dos Santos Felix <felixs.rodrigo@gmail.com>
2024-12-17 21:45:24 -0300

    refactor: normalize client repository

--- src/client/adapters/postgresql_repository.py
+++ src/client/adapters/postgresql_repository.py
@@ -1,11 +1,21 @@
+import uuid
 from typing import Dict, Sequence, Optional
 
 from sqlalchemy import select, Row, delete
 from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.orm import Session
 
-from src.client.adapters.client_table import ClientTable
+from src.client.adapters.client_table import ProfileTable, CredentialTable
 from src.client.ports.repository_interface import IClientRepository
+
+PROFILE_COLS: tuple = (
+    ProfileTable.id,
+    ProfileTable.first_name,
+    ProfileTable.last_name,
+    ProfileTable.cpf,
+    CredentialTable.email,
+    CredentialTable.password,
+)
 
 
 class PostgreSqlClientRepository(IClientRepository):
@@ -14,12 +24,7 @@ class PostgreSqlClientRepository(IClientRepository):
         self.session = session
 
     def get_users(self) -> Optional[Sequence[Row[tuple]]]:
-        stmt = select(ClientTable.id,
-                      ClientTable.first_name,
-                      ClientTable.last_name,
-                      ClientTable.cpf,
-                      ClientTable.email,
-                      ClientTable.password)
+        stmt = select(*PROFILE_COLS).join(ProfileTable.credential)
         results = self.session.execute(stmt).all()
         if not results:
             return []
@@ -27,12 +32,7 @@ class PostgreSqlClientRepository(IClientRepository):
         return results
 
     def get_user_by_cpf(self, cpf: str) -> Optional[Row]:
-        stmt = select(ClientTable.id,
-                      ClientTable.first_name,
-                      ClientTable.last_name,
-                      ClientTable.cpf,
-                      ClientTable.email,
-                      ClientTable.password).where(ClientTable.cpf == cpf)
+        stmt = select(*PROFILE_COLS).join(ProfileTable.credential).where(ProfileTable.cpf == cpf)
         results = self.session.execute(stmt).first()
         if not results:
             return
@@ -40,12 +40,7 @@ class PostgreSqlClientRepository(IClientRepository):
         return results
 
     def get_user_by_email(self, email: str) -> Optional[Row]:
-        stmt = select(ClientTable.id,
-                      ClientTable.first_name,
-                      ClientTable.last_name,
-                      ClientTable.cpf,
-                      ClientTable.email,
-                      ClientTable.password).where(ClientTable.email == email)
+        stmt = select(*PROFILE_COLS).join(ProfileTable.credential).where(CredentialTable.email == email)
         results = self.session.execute(stmt).first()
         if not results:
             return
@@ -53,28 +48,45 @@ class PostgreSqlClientRepository(IClientRepository):
         return results
 
     def create_user(self, user: Dict):
-        stmt = insert(ClientTable).values(**user)
-        self.session.execute(stmt)
+        credential = self.save_credential(email=user['email'], password=user['password'])
+
+        profile = ProfileTable(credential_id=credential.id, **user)
+        self.session.add(profile)
+        self.session.commit()
+
+    def save_credential(self, email: str, password: str):
+        credential = CredentialTable(id=str(uuid.uuid4()), email=email, password=password)
+        self.session.add(credential)
+        self.session.commit()
+        return credential
 
     def update_user(self, user: Dict):
-        stmt = insert(ClientTable).values(**user)
+        profile_data = {key: user[key] for key in user if key not in ['email', 'password']}
+        stmt = insert(ProfileTable).values(**profile_data)
         stmt = stmt.on_conflict_do_update(
-            index_elements=[ClientTable.id],
-            set_={key: user[key] for key in user if key != 'id'}
+            index_elements=[ProfileTable.id],
+            set_=profile_data
         )
         self.session.execute(stmt)
 
+        credential_data = {key: user[key] for key in user if key in ['email', 'password']}
+        if credential_data:
+            stmt = insert(CredentialTable).values(id=user['credential_id'], **credential_data)
+            stmt = stmt.on_conflict_do_update(
+                index_elements=[CredentialTable.id],
+                set_=credential_data
+            )
+            self.session.execute(stmt)
+
+        self.session.commit()
+
     def delete_user(self, user_id: str):
-        stmt = delete(ClientTable).where(ClientTable.id == user_id)
+        stmt = delete(ProfileTable).where(ProfileTable.id == user_id)
         self.session.execute(stmt)
+        self.session.commit()
 
     def get_user_by_id(self, user_id: str) -> Optional[Row]:
-        stmt = select(ClientTable.id,
-                      ClientTable.first_name,
-                      ClientTable.last_name,
-                      ClientTable.cpf,
-                      ClientTable.email,
-                      ClientTable.password).where(ClientTable.id == user_id)
+        stmt = select(*PROFILE_COLS).join(ProfileTable.credential).where(ProfileTable.id == user_id)
         results = self.session.execute(stmt).first()
         if not results:
             return
